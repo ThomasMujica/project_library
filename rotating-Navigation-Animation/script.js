bb293c75a56caecb71f8f5add2f0f34467b49165
Thomas Mujica <temc.business@gmail.com>
2023-09-25 13:56:24 -0300

    Mejorando visibilidad del Menu

--- rotating-Navigation-Animation/script.js
+++ rotating-Navigation-Animation/script.js
@@ -1,7 +1,12 @@
 const open = document.getElementById("open");
 const close = document.getElementById("close");
 const container = document.querySelector(".rotatingNavigationContainer");
+const prueba = document.querySelector(".prueba");
 
 open.addEventListener("click", () => container.classList.add("show-nav"));
 
 close.addEventListener("click", () => container.classList.remove("show-nav"));
+
+open.addEventListener("click", () => prueba.classList.add("mostrar"));
+
+close.addEventListener("click", () => prueba.classList.remove("mostrar"));
